e62949ebe6b4af6593b7fc1fe24089975eda36fb
masholomidze <lomidzem2011@gmail.com>
2024-03-11 18:18:51 +0400

    commit

--- python course/day 08/homework/droshimogzauroba.py
+++ python course/day 08/homework/droshimogzauroba.py
@@ -2,5 +2,5 @@ age = int(input("how old are you?: "))
 travel = int(input("how many years later do you want to travel?: "))
 travel_age = age + travel
 
-print(str(travel) + "years later you will be" + str(travel_age) + "year old")
+print(str(travel) + " years later you will be " + str(travel_age) + " year old")
 
---
+++ python course/day 10/homework/andor.py
@@ -0,0 +1,11 @@
+number1 = int(input("number 1:" ))
+number2 = int(input("number 2:" ))
+number3 = int(input("number 3:" ))
+
+answer1 = number1 < number2
+answer2 = number2 < number3
+
+print(answer1)
+print(answer2)
+print(answer1 and answer2)
+print(answer1 or answer2)
