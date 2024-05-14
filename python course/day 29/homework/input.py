36f171bca0472dceb626c2617fe20aa83431fde7
masholomidze <lomidzem2011@gmail.com>
2024-05-14 17:52:11 +0400

    commit

---
+++ python course/day 29/homework/input.py
@@ -0,0 +1,19 @@
+best_academy = "goa"
+answer= "yes"
+answer1 = "no"
+question = input("witch is the best academy ever?")
+
+
+while question ! = best_academy:
+   print("are you sure. Try again")
+   question = input("witch is the best academy ever?")
+
+
+print("you are right")
+question1 = input("do you know goa?")
+while question1 ! = answer:
+   print("are you sure.i give you another chanse")
+   question1 = inputut("do you know goa?")
+
+   if question1 == answer:
+      print(good. then join us if you arent joined)
