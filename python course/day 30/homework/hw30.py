65f5635cd29e68ee2c8b915e00141fc609fd808b
masholomidze <lomidzem2011@gmail.com>
2024-05-25 20:59:52 +0400

    commit

--- python course/day 30/homework/hw30.py
+++ python course/day 30/homework/hw30.py
@@ -1,3 +1,3 @@
 car = ["porsche", "range rover", "toyota"]
 
-print("My favorite car is " + car[0] + ", i have" + car[1] + "and i hate" + car[2])
+print("My favorite car is " + car[0] + ", i have " + car[1] + " and i hate " + car[2])
