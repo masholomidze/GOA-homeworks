49b3f2b2a2fa89f8034fe2f843263ac06bf417a2
masholomidze <lomidzem2011@gmail.com>
2024-05-28 21:15:25 +0400

    commit

---
+++ python course/day 32/homework/hw32.py
@@ -0,0 +1,7 @@
+goa = "Goa is the best sdasdasd adasdasd asdasdasdasdasd asdasdasd asd"
+arr = [1,2,3,3,4,4,4,4,5,5,5,6,6,7,78,8]
+new_goa = ""
+for char in range(len(goa)):
+    if goa[char] != " " :
+        new_goa += goa[char]
+print(new_goa)
